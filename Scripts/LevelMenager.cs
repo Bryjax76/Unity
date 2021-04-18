2c5f3fc5babcc117318e17e9bcac9640656b21f6
Bryjax76 <66446836+Bryjax76@users.noreply.github.com>
2021-04-18 20:21:59 +0200

    Add files via upload
    SunnyLand Assets my really first game. Just scripts.

---
+++ Scripts/LevelMenager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelMenager : MonoBehaviour
+{
+    public static LevelMenager instance;
+    public Transform respawnPoint;
+    public GameObject playerPrefab;
+
+    public void Awake()
+    {
+        instance = this;
+    }
+
+    public void Respawn()
+    {
+        Instantiate(playerPrefab, respawnPoint.position, Quaternion.identity);
+    }
+}
